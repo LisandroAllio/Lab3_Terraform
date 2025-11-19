a8ed278503664905983551e6d7f0f668a9f6c3d0
LaraSperanza <larusp8@gmail.com>
2025-11-19 18:59:05 -0300

    Fix: orden de carpetas

--- environments/dev/main.tf
+++
@@ -1,9 +0,0 @@
-module "ecr" {
-    source = "../../modules/ecr"
-
-    name_front = "lab/front"
-    name_bd = "lab/bd-mysql"
-    image_tag_mutability = "MUTABLE"
-    encryption_type = "AES256"
-    scan_on_push = false
-}
