3dd50ee0ec6145e51561d1ee548726e55483da0e
LisandroAllio <lichiallio@gmail.com>
2025-11-19 10:47:56 -0300

    fix in module s3

--- management/backend/main.tf
+++ management/backend/main.tf
@@ -1,40 +1,19 @@
-#S3 Bucket for Terraform State
-resource "aws_s3_bucket" "backend" {
-  bucket = "s3-backend-teralab3-grupo-1"
+terraform {
+  required_version = ">= 1.0"
 
-  tags = {
-    Name        = "Terraform State Bucket"
-    Environment = "dev"
-    Owner       = "federico"
-  }
-}
-
-# Enable versioning to keep history of state files
-resource "aws_s3_bucket_versioning" "backend" {
-  bucket = aws_s3_bucket.backend.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-# Enable server-side encryption by default
-resource "aws_s3_bucket_server_side_encryption_configuration" "backend" {
-  bucket = aws_s3_bucket.backend.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
-}
 
-# Block public access to the bucket
-resource "aws_s3_bucket_public_access_block" "backend" {
-  bucket = aws_s3_bucket.backend.id
+  backend "s3" {
+    bucket = "s3-backend-teralab3-grupo-1"
+    key    = "backend/terraform.tfstate"
+    region = "us-east-1"
+    use_lockfile = true
+    encrypt = true
+  }
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
 }
--- management/backend/provider.tf
+++ management/backend/provider.tf
@@ -1,23 +1,3 @@
-terraform {
-  required_version = ">= 1.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
-  backend "s3" {
-    bucket = "s3-backend-teralab3-grupo-1"
-    key    = "backend/terraform.tfstate"
-    region = "us-east-1"
-    use_lockfile = true
-    encrypt = true
-  }
-
-}
-
 provider "aws" {
   region = "us-east-1"
 }
