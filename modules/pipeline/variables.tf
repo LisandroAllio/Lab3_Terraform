e0fdd8868f926e3c82ae3b5853a6074135f35df0
LisandroAllio <lichiallio@gmail.com>
2025-11-23 18:16:00 -0300

    Modulo de pipeline base completo | Roles yu Bucket creado | Modificaiones en codebuild

---
+++ modules/pipeline/variables.tf
@@ -0,0 +1,39 @@
+variable "codebuild_project_name" {
+  description = "Name of the CodeBuild project to use in the pipeline"
+  type        = string
+}
+
+variable "s3_bucket_name" {
+  description = "Name for the S3 bucket to store pipeline artifacts"
+  type        = string
+}
+
+variable "repository_id" {
+  description = "GitHub repository ID in format owner/repo"
+  type        = string
+  default     = "federicogfb/teralab2"
+}
+
+variable "ecs_cluster_name" {
+  description = "Name of the ECS cluster for deployment"
+  type        = string
+  default     = "lab-cluster"
+}
+
+variable "ecs_service_name" {
+  description = "Name of the ECS service for deployment"
+  type        = string
+  default     = "front-task-service"
+}
+
+variable "aws_region" {
+  description = "AWS region for resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "aws_account_id" {
+  description = "AWS account ID"
+  type        = string
+  default     = "979244568430"
+}
