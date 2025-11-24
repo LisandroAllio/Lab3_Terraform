01737c0fb9d8d68896f14c404f4358720d8e2cce
LisandroAllio <lichiallio@gmail.com>
2025-11-24 13:06:49 -0300

    cambios en role | todavia no hay solucion

--- 01_environments/dev/pipeline.tf
+++ 01_environments/dev/pipeline.tf
@@ -14,5 +14,11 @@ module "codebuild_project" {
 module "codepipeline" {
   source = "../../modules/pipeline"
   codebuild_project_name = module.codebuild_project.codebuild_project_name
+  codepipeline_role_name = "lab-front-pipeline-role"
   s3_bucket_name = "lab3-pipeline-bucket"
+  aws_region = "us-east-1"
+  aws_account_id = "979244568430"
+  ecs_cluster_name = "lab3-cluster"
+  ecs_service_name = "frontend-service"
+  repository_id = "federicogfb/teralab2"
 }
