9cf5b6b075af14d45ea9cb5e74105990d8796b68
federicogfb <fedelibra98@gmail.com>
2025-11-25 15:54:30 -0300

    Fix: swapped the value of email_addresses from pipeline_notification_emails to a string list with plain email addresses

--- 01_environments/dev/pipeline.tf
+++ 01_environments/dev/pipeline.tf
@@ -5,16 +5,16 @@
 }*/
 
 #### Variable para emails de notificaciones ####
-variable "pipeline_notification_emails" {
-  description = "Lista de direcciones de correo para recibir notificaciones del pipeline. IMPORTANTE: Después de crear el recurso, cada email recibirá un correo de confirmación de suscripción que debe ser confirmado haciendo clic en el enlace del correo."
-  type        = list(string)
-  default     = [
-    "eze.castelnuovo@gmail.com",
-    "fedelibra98@gmail.com",
-    "alliolisandro@gmail.com",
-    "lara.speranza6@gmail.com"
-  ]
-}
+# variable "pipeline_notification_emails" {
+#   description = "Lista de direcciones de correo para recibir notificaciones del pipeline. IMPORTANTE: Después de crear el recurso, cada email recibirá un correo de confirmación de suscripción que debe ser confirmado haciendo clic en el enlace del correo."
+#   type        = list(string)
+#   default     = [
+#     "eze.castelnuovo@gmail.com",
+#     "fedelibra98@gmail.com",
+#     "alliolisandro@gmail.com",
+#     "lara.speranza6@gmail.com"
+#   ]
+# }
 
 #### SNS Topic para Notificaciones del Pipeline ####
 module "sns_pipeline_notifications" {
@@ -22,7 +22,12 @@ module "sns_pipeline_notifications" {
 
   name_prefix      = "lab3-pipeline"
   display_name     = "Lab3 Pipeline Notifications"
-  email_addresses  = var.pipeline_notification_emails
+  email_addresses  = [
+    "eze.castelnuovo@gmail.com",
+    "fedelibra98@gmail.com",
+    "alliolisandro@gmail.com",
+    "lara.speranza6@gmail.com"
+  ]
   environment      = "dev"
   aws_account_id   = "979244568430"
 
