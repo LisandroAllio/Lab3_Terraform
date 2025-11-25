683f6fe73cbc936b70a9cba57ceec54bd6bfa4e7
ezequielcastelnuovo <eze.castelnuovo@gmail.com>
2025-11-25 13:25:58 -0300

    refactor: consolidar configuración SNS en pipeline.tf y eliminar redundancias

--- 01_environments/dev/pipeline.tf
+++ 01_environments/dev/pipeline.tf
@@ -4,6 +4,34 @@
   id = "arn:aws:codeconnections:us-east-1:979244568430:connection/4559c724-bd4c-4af9-929b-8d1448d57c7b"
 }*/
 
+#### Variable para emails de notificaciones ####
+variable "pipeline_notification_emails" {
+  description = "Lista de direcciones de correo para recibir notificaciones del pipeline. IMPORTANTE: Después de crear el recurso, cada email recibirá un correo de confirmación de suscripción que debe ser confirmado haciendo clic en el enlace del correo."
+  type        = list(string)
+  default     = [
+    "eze.castelnuovo@gmail.com",
+    "fedelibra98@gmail.com",
+    "alliolisandro@gmail.com",
+    "lara.speranza6@gmail.com"
+  ]
+}
+
+#### SNS Topic para Notificaciones del Pipeline ####
+module "sns_pipeline_notifications" {
+  source = "../../modules/sns"
+
+  name_prefix      = "lab3-pipeline"
+  display_name     = "Lab3 Pipeline Notifications"
+  email_addresses  = var.pipeline_notification_emails
+  environment      = "dev"
+  aws_account_id   = "979244568430"
+
+  tags = {
+    Name  = "Pipeline Notifications"
+    Owner = "Ezequiel"
+  }
+}
+
 module "codebuild_project" {
     source = "../../modules/code_build"
     codebuild_iam_role_name = "codebuild-role-name"
